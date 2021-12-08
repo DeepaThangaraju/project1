9d2c9d0c57f2c547403b6eec176d6f8b482395ee
DeepaThangaraju <deepat2405@gmail.com>
2021-12-08 12:51:35 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@mui/material/TextField";
 
 function App() {
+  const profile = [
+    {
+      Firstname: "Deepa",
+      lastname: "Thangaraju",
+      DateOfBirth: "24.05.1994",
+      Qualification: "B.E-Computer Science",
+      passedoutyear: 2016,
+      Email_Id: "deepat2405@gmail.com",
+      Gender: "female",
+      phoneno: 1234567890,
+      experience: "0 years",
+      Photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc2TelJFZFY4gYoFdkh7tuC1PxyWF7ERibag&usqp=CAU",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data profile={profile} />
     </div>
+  );
+}
+
+function Data({ profile }) {
+  return (
+    <div>
+      {profile.map((profile, index) => (
+        <Display
+          fname={profile.Firstname}
+          lname={profile.lastname}
+          pic={profile.Photo}
+          dob={profile.DateOfBirth}
+          degree={profile.Qualification}
+          poy={profile.passedoutyear}
+          mail={profile.Email_Id}
+          no={profile.phoneno}
+          exp={profile.experience}
+          Gender={profile.Gender}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Display({
+  fname,
+  lname,
+  pic,
+  dob,
+  degree,
+  poy,
+  mail,
+  no,
+  exp,
+  Gender,
+}) {
+  const sty = {
+    height: "1rem",
+    width: "70%",
+    margin: "2rem 0rem",
+    color: "white",
+  };
+  return (
+    <section>
+      <div className="display">
+        <img src={pic} alt={fname} />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={fname}
+          label="First Name"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={lname}
+          label="Last Name"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={dob}
+          label="Date Of Birth"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={degree}
+          label="Qualification"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={poy}
+          label="Passed Out Year"
+          variant="outlined"
+          style={sty}
+        />
+        <TextField
+          id="outlined-disabled"
+          value={Gender}
+          label="Qualification"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={mail}
+          label="Email"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={no}
+          label="Phone no"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+        <TextField
+          id="outlined-disabled"
+          value={exp}
+          label="Experience"
+          variant="outlined"
+          style={sty}
+        />
+        <br />
+      </div>
+    </section>
   );
 }
 
